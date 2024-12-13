957e11c880342cf6b2009c98852214e95b70df47
Aleksey Manzhikov <sanal-manzhikov@mail.ru>
2024-12-13 03:48:07 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,6 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv('POSTGRES_HOST'),
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
